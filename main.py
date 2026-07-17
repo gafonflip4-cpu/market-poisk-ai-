e59043b70654609d0a916dbd92709da48094dcc9
gafonflip4-cpu <gafonflip4@gmail.com>
2026-07-17 21:00:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask
 import os
+import telebot
+from flask import Flask, request
 
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message, "Привет! Я бот для поиска товаров на маркетплейсах.")
 
 @app.route('/')
 def home():
     return "Бот работает!"
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return 'ok', 200
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
